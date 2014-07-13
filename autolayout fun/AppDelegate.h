e89fcfd32cf4f342e3fbf01e8c1c352fac5c9a59
otiose <otiose.code@gmail.com>
2014-07-13 12:37:20 +0900

    Initial Commit

---
+++ autolayout fun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  autolayout fun
+//
+//  Created by otiose on 7/13/14.
+//  Copyright (c) 2014 otiose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ autolayout funTests/autolayout_funTests.m
@@ -0,0 +1,39 @@
+//
+//  autolayout_funTests.m
+//  autolayout funTests
+//
+//  Created by  otiose on 7/13/14.
+//  Copyright (c) 2014 otiose. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface autolayout_funTests : XCTestCase
+
+@end
+
+@implementation autolayout_funTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
